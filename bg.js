ac8fafcd41d80fe1b211e6e6966a222bdb35cce0
vicitpsn <vichanir9k@gmail.com>
2015-11-11 03:01:18 +0100

    Create bg.js

---
+++ bg.js
@@ -0,0 +1,41 @@
+Cufon.replace('.copyrights p', {
+    fontFamily: 'PF Premier Text Medium',
+    fontWeight: '400',
+    fontSize: '13pt',
+	color: '#fff'
+});
+
+Cufon.replace('.content-cufon', {
+    fontFamily: 'PF Premier Text Medium',
+    fontWeight: '400',
+    fontSize: '16pt',
+	color: '#666'
+});
+
+Cufon.replace('.shelf-text', {
+    fontFamily: 'PF Premier Text Medium',
+    fontWeight: '400',
+    fontSize: '22pt',
+	color: '#666'
+}); 
+
+Cufon.replace('h1', {
+    fontFamily: 'PF Premier Text Medium',
+    fontWeight: '400',
+    fontSize: '20pt',
+	color: '#666'
+}); 
+
+Cufon.replace('h2', {
+    fontFamily: 'PF Premier Text Medium',
+    fontWeight: '400',
+    fontSize: '16pt',
+	color: '#666'
+}); 
+
+Cufon.replace('#home-text', {
+    fontFamily: 'PF Premier Text Medium',
+    fontWeight: '400',
+    fontSize: '25pt',
+	color: '#666'
+}); 
